5f7275ce83d88772519d22335dda539da7bef604
RAlanWright <wright.alan88@gmail.com>
2019-04-09 20:41:04 -0500

    Fixed formatting and code execution

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from caesar import rotate_string   
 
 app = Flask(__name__)
-app.config['DEBUG']
+app.config['DEBUG'] = True
 
 form = """
 <!DOCTYPE html>
@@ -10,48 +10,48 @@ form = """
 <html>
     <head>
         <style> 
-            form {
+            form {{
                 background-color:#eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
-        <form method="POST">
-            <label for="rot">Rotate by:</label>
-            <input type="text" name="rot" value="0">
-            
-            <textarea type="text" name="text"></textarea>
-
+        <form method="post">
+            <div>
+                <label for="rot">Rotate by:</label>
+                <input type="text" name="rot" value="0">
+            </div>
+            <textarea name="text">{0}</textarea>
+            <br>
             <input type="submit">
         </form>
-        
-        
-
     </body>
 </html>
-
-
 """
 
 @app.route("/" )
 def index():
-    return form 
+    return form.format("") 
 
-app.run()
+
 
 @app.route("/", methods=['POST'])
 def encrypt():
-    rot = int(request.form["rot"])
+    rot = request.form["rot"]
     text = request.form["text"]
-    rotate_string(text,rot)
-    return encrypt
+
+    rot = int(rot)
+    text = rotate_string(text,rot)
+    return form.format(text)
+
+app.run()
